ff3033be634ea9ecbae9334ff9ac890dbdbd7193
taiwenlee <leetaiwen101@gmail.com>
2022-04-20 14:17:20 -0700

    base code

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// Endless runner game for CMPM/ARTG 120 by Tai Wen Lee, 
+// Jake Indgin, Lily Pham, and Rachel Trieu
+
+let config = {
+   type: Phaser.CANVAS,
+   width: 640,
+   height: 480,
+   scene: [Menu]
+}
+
+let game = new Phaser.Game(config);
