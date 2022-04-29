616f1b1940563f89f48664bb813af655194dcdae
rachbound <rtrieu@ucsc.edu>
2022-04-28 18:31:47 -0700

    added button images

--- scenes/Settings.js
+++ scenes/Settings.js
@@ -26,7 +26,7 @@ class Setting extends Phaser.Scene {
       exitButton.tint = 0xff0000; // temp tinting for the button
 
       // temp music volume up button
-      const musicVolumeUpButton = this.add.image(500, 200, 'button').setOrigin(0.5);
+      const musicVolumeUpButton = this.add.image(400, 300, 'button').setOrigin(0.5);
       musicVolumeUpButton.setInteractive();
       musicVolumeUpButton.on('pointerdown', () => {
          this.selectSound.play({volume: sfxVol});
@@ -41,7 +41,7 @@ class Setting extends Phaser.Scene {
       musicVolumeUpButton.tint = 0x00ff00; // temp tinting for the button
 
       // temp music volume down button
-      const musicVolumeDownButton = this.add.image(500, 300, 'button').setOrigin(0.5);
+      const musicVolumeDownButton = this.add.image(200, 300, 'button').setOrigin(0.5);
       musicVolumeDownButton.setInteractive();
       musicVolumeDownButton.on('pointerdown', () => {
          this.selectSound.play({volume: sfxVol});
@@ -56,7 +56,7 @@ class Setting extends Phaser.Scene {
       musicVolumeDownButton.tint = 0x00ff00; // temp tinting for the button
 
       // temp sfx volume up button
-      const sfxVolumeUpButton = this.add.image(500, 400, 'button').setOrigin(0.5);
+      const sfxVolumeUpButton = this.add.image(400, 500, 'button').setOrigin(0.5);
       sfxVolumeUpButton.setInteractive();
       sfxVolumeUpButton.on('pointerdown', () => {
          this.selectSound.play({volume: sfxVol});
@@ -70,7 +70,7 @@ class Setting extends Phaser.Scene {
       sfxVolumeUpButton.tint = 0x00ff00; // temp tinting for the button
 
       // temp sfx volume down button
-      const sfxVolumeDownButton = this.add.image(500, 500, 'button').setOrigin(0.5);
+      const sfxVolumeDownButton = this.add.image(200, 500, 'button').setOrigin(0.5);
       sfxVolumeDownButton.setInteractive();
       sfxVolumeDownButton.on('pointerdown', () => {
          this.selectSound.play({volume: sfxVol});
